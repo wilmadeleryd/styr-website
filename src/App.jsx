6908d4b9ce1670c0e209ee1093aef06fb0b529cc
Wilma Deleryd <wilma@styrmedia.se>
2026-06-29 14:18:41 +0200

    Add ScrollToTop on navigation, more mobile accordion padding

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './pages/Home'
 import CaseMila from './pages/CaseMila'
 import Contact from './pages/Contact'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => { window.scrollTo(0, 0) }, [pathname])
+  return null
+}
+
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen font-body">
+      <ScrollToTop />
       <Navbar />
       <main className="flex-grow">
         <Routes>
